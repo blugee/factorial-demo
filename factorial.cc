b7b428063c5782542534ccdce169f4fe30088806
patel243 <nayanlodaliya243@gmail.com>
2019-06-30 13:03:24 +0530

    --update code

--- factorial.cc
+++ factorial.cc
@@ -1,47 +1,44 @@
 #include <node.h>
 
-// double factorial(long long int n) {
-//   if (n == 0)
-//     return 1;
-//   return n * factorial(n - 1);
-// }
-
-#define size 10000 
-
-int fact(int x, int ar[], int ar_size);
-
-void factorial(int n)
+/*double factorial(double n) {
+  if (n == 0)
+    return 1;
+  return n * factorial(n - 1);
+}*/
+using namespace std;
+ 
+int multiply(int x,int a[],int size)
 {
-  int ar[size];
-  ar[0] = 1;
-  int ar_size = 1;
-  
-  for (int x=2; x<=n; x++)
-    ar_size = fact(x, ar, ar_size);
-  
-  for (int i=ar_size-1; i>=0; i--)
-    cout << ar[i];  
-
-  cout<<endl;
-}
-
-int fact(int x, int ar[], int ar_size)
-{
-  int c = 0; 
-  for (int i=0; i< ar_size; i++)
-  {   int p = ar[i] * x + c;
-    ar[i] = p % 10; 
-    c = p/10; 
-  }
-
-  while (c)
+  int carry=0,i,p;
+    
+  for(i=0;i<size;++i)
   {
-    ar[ar_size] = c%10;
-    c = c/10;
-    ar_size++;
+    p=a[i]*x+carry;
+    a[i]=p%10;
+    carry=p/10;
   }
-  return ar_size;
-  
+    
+  while(carry!=0)
+  {
+    a[size]=carry%10;
+    carry=carry/10;
+    size++;   
+  }     
+      return size;
+}
+int factorial(int n){
+  int a[1000],i,size=1;
+    a[0]=1;
+ 
+    for(i=2;i<=n;++i) {
+      size=multiply(i,a,size);        
+    }
+      
+    // for(i=size-1;i>=0;--i)
+    // {
+    //   cout<<a[i];     
+    // }
+    return a;
 }
 
 void factorial(const v8::FunctionCallbackInfo<v8::Value>& info) {
